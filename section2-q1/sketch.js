758d5834e210a1dc9d30431e8ae0fcd874bfdf64
elmekr <114969988+elmekr@users.noreply.github.com>
2022-10-18 12:30:24 +0900

    update 2-q1
    finished

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,15 @@
 // 小手調べ
 function setup(){
+  createCanvas(100, 100)
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    noFill();
+    let size = (i + 1) * 10
+        if(i < 5){
+          stroke(0, 0, 255)
+        }
+        else{
+          stroke(255, 0, 0)
+        }
+    ellipse(50, 50, size);
   }
 }
