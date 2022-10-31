2c534214e96565e1f4144b46b0491a93fe3ea5f4
elmekr <114969988+elmekr@users.noreply.github.com>
2022-11-01 07:58:48 +0900

    some extent

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,13 +4,20 @@ let cycle;
 
 function setup(){
   createCanvas(200, 200);
-  count = 0;
+  count = 50;
   cycle = 100;
 }
 
 function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
-  // BLANK[1]
+  let size;
+  size = 10
+  if(count >= 50){
+    size = count
+  }
+  else{
+    size = cycle - count
+  }
   ellipse(width / 2, height / 2, size);
 }
