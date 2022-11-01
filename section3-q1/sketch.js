060f121981a573bf0c961d29559d656a7771dbfe
elmekr <114969988+elmekr@users.noreply.github.com>
2022-11-01 11:21:55 +0900

    finished

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,23 +1,27 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
-
 function setup(){
   createCanvas(200, 200);
+  stroke(255, 0, 0)
   count = 50;
   cycle = 100;
 }
 
 function draw(){
-  background(160, 192, 255);
-  count = (count + 1) % cycle;
+  background(255, 150, 150);
+  if ( keyIsDown( ' '.charCodeAt(0) ) ){
+    count = (count + 1.5) % cycle;
+  }
+  else{
+    count = (count + 1) % cycle;
+  }
   let size;
   size = 10
-  if(count >= 50){
+  if(count >= 40){
     size = count
   }
   else{
-    size = cycle - count
+    size = cycle - 1.5 * count
   }
+  fill(255, 125, 125)
   ellipse(width / 2, height / 2, size);
 }
