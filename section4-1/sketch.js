6c79ce47509130b5fa4d8d0d143ae1996bb91248
elmekr <114969988+elmekr@users.noreply.github.com>
2022-11-01 12:08:14 +0900

    finished required task

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -6,7 +6,7 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
   }
   console.log(scores);
 
@@ -18,18 +18,28 @@ function setup(){
   console.log(sum);
 
   // ここから平均・最大・最小を求めます
-  let average, largest, smallest;
-  // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
-
-  largest = 0;
+  let average = 0
   for(let i = 0; i < scores.length; i++){
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+    average = sum / i
   }
+  console.log(average);
 
-  smallest = 100;
+  let  largest = 0;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[3]　ヒント：最小値とだいたい同じ
+    if ( scores[i] > largest {
+      largest = scores[i]
+    }
+   } // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+  console.log(largest);
+
+  let smallest = 100;
+  for(let i = 0; i < scores.length; i++){
+    if( scores[i] < largest ){
+      smallest = scores[i]
+    }CanvasCaptureMediaStreamTrackc
   }
+  console.log(smallest);
+
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
@@ -40,7 +50,7 @@ function setup(){
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
-    // BLANK[4] ヒント: 条件分岐を使って色を変更します
+   // BLANK[4] ヒント: 条件分岐を使って色を変更します
     rect(i * dx + 2, height - h, dx - 4, h);
     fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
